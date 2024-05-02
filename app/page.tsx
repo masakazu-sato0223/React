41d5a5813f37db62830f87592a185c92bc84ba57
masakazu-sato0223 <masakazukasama223@gmail.com>
2024-05-02 22:57:31 +0900

    初回

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './globals.css';
+import MyPractice from './MyPractice';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Reactの練習</h1>
+    </div>
+  );
+}
+
+export default App;
